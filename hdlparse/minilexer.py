269f74345aaf5232ea21e4330543f97dbea8eba7
Dmitry Murzinov <kakstattakim@gmail.com>
2024-02-21 00:33:52 +0400

    merge https://github.com/hdl/pyHDLParser/pull/10

--- hdlparse/minilexer.py
+++ hdlparse/minilexer.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 # Copyright © 2017 Kevin Thibedeau
 # Distributed under the terms of the MIT license
+"""Minimalistic lexer engine inspired by the PyPigments RegexLexer"""
 import re
 import logging
 
-"""Minimalistic lexer engine inspired by the PyPigments RegexLexer"""
-
-__version__ = '1.0.7'
 
 log = logging.getLogger(__name__)
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s'))
-log.addHandler(handler)
+
+if not log.handlers:  # only add the handler if no handlers are already registered
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s'))
+    log.addHandler(handler)
 
 
-class MiniLexer(object):
+class MiniLexer():
     """Simple lexer state machine with regex matching rules"""
 
     def __init__(self, tokens, flags=re.MULTILINE):
@@ -53,7 +53,7 @@ class MiniLexer(object):
           text (str): Text to apply lexer to
 
         Yields:
-          A sequence of lexer matches.
+          A sequence of lexer matches
         """
 
         stack = ['root']
@@ -66,7 +66,7 @@ class MiniLexer(object):
                 m = pat.match(text, pos)
                 if m:
                     if action:
-                        log.debug(f"Match: {m.group().strip()} -> {action}")
+                        log.debug("Match: %s -> %s", m.group().strip(), action)
 
                         yield (pos, m.end() - 1), action, m.groups()
 
